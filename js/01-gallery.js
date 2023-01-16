46c2f5965aa903b35af21b35d80d5a886ab6c810
Maksym Kikot <maxym1111111@gmail.com>
2023-01-16 17:39:42 +0200

    first task has done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsMakerMarkup = galleryItems
+  .map(image => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsMakerMarkup);
+
+galleryEl.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(e) {
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  e.preventDefault();
+
+  const instance = basicLightbox.create(`<img src="${e.target.dataset.source}">`, {
+    onShow: instance => {
+      document.onkeydown = function (e) {
+        e.preventDefault();
+        if (e.code == 'Escape') {
+          instance.close();
+        }
+      };
+    },
+  });
+
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const arrayGalleryItems = galleryItems
+  .map(item => {
+    return `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>`;
+  })
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', arrayGalleryItems);
+
+galleryEl.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(e) {
+  e.preventDefault();
+
+  var lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionSelector: e.target.alt,
+    captionDelay: 250,
+  });
+}
